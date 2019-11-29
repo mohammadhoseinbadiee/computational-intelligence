e3ef0afc45c6120d7d71e65e43069fd85e6221e9
mohammadhoseinbadiee <58256853+mohammadhoseinbadiee@users.noreply.github.com>
2019-11-29 19:45:08 +0330

    Add files via upload

--- main.py
+++ main.py
@@ -1,168 +1,107 @@
-from MyKmean import Kmean
-
-# from mpl_toolkits.mplot3d import Axes3D
-# import matplotlib.pyplot as plt
-# from matplotlib import cm
-# from matplotlib.ticker import LinearLocator, FormatStrFormatter
 import numpy as np
+import cv2
 
-class Gaussian:
-    def __init__(self ,x_mean , sigma):
-        self.mean = x_mean
-        self.sigma = sigma
-    
-    def dist(self , p1 , p2):
-        dd = np.array(p1) - np.array(p2)
-        return np. sqrt(dd[0]**2 + dd[1]**2)
+class Kmean:
+	def __init__(self , cluster_num , input_dim):
+		self.cluster_num = cluster_num
 
-    def eval(self, x):
-        aa =  pow((1/(self.sigma*np.sqrt(2*np.pi)))*np.exp(-0.5*(self.dist(x,self.mean))/(self.sigma)),2);
-        # print(aa)
-        return aa
+		self.C =[]# 300*np.random.random([cluster_num , input_dim])
+		# for i in range(cluster_num):
+		# 	self.C.append(np.array([np.random.randint(255), np.random.randint(255), np.random.randint(255)]))
+		# self.C = np.array(self.C)
+		self.input_dim = input_dim
+		self.clusters = []
 
+	def setRand(self):
+		for i in range(self.cluster_num):
+			self.C.append(self.input_data[np.random.randint(9000)])
 
-class perceptron:
-    def __init__(self , m):
-        m = m+1
-        # self.threshold = np.random.random()
-        self.w =  np.random.random(m)
-        self.eta = .25
-        self.Err = 0
+	def setClusterNum(self,cluster_num):
+		self.cluster_num = cluster_num;
+		self.__init__(self.cluster_num, self.input_dim)
 
-    def eval(self,x):
-        x = np.array(x)
-        x = np.append(x , -1)
-        net = np.sum((x*self.w))
-        return net
+	def setInputDimension(self , input_dim):
+		self.input_dim = input_dim 
+		self.__init__(self.cluster_num , input_dim)
 
-    def train__(self , x , d):
-        x = np.array(x)
-        y = np.append(x , -1)
-        o = self.eval(x)
-        self.w = self.w + 0.5*self.eta*(d - o)*y##*(1 - o**2)
-        self.Err = self.Err + (d-o)**2;
-        return self.Err
+	def dist(self,p1,p2):
+		return np.sqrt(np.sum((p2-p1)**2))
 
-    def train(self , input_x , input_y , n):
-        Emax = 3.1
-        while(1):
-            # print("1111")
-            err = 0
-            for i in range(n):
-                x = input_x[i]
-                y = input_y[i]
-                err = self.train__(x,y)
-            
-            print(err)
-            if err < Emax:
-                break
-            else:
-                self.Error_reset()
+	def centerOfMass(self , data_arr):
+		s = 0;
+		n = 0;
+		# print(data_arr)
+		for i in data_arr:
+			s = s + np.float64(i)
+			n = n+1;
+		# print(s)
+		if (n==0):
+			return np.array([-1000,-1000,-1000])
+		return (1.0/n)*s;
 
-    def Error_reset(self):
-        self.Err = 0
+	def train(self, input_data):
+		self.input_data = input_data
+		self.setRand()
+		for i in range(10):
+			self.train_(input_data)
 
+	def train_(self , input_data):
+		self.lastClusters = self.clusters.copy
+		self.clusters = [[] for i in range(self.cluster_num)];
+		for data in input_data:
+			d , last_d = 0 , 100000000;
+			nearest_ci = 0;
+			for ci in range(self.cluster_num):
+				d = self.dist(data, self.C[ci])
+				if d < last_d:
+					last_d = d ;
+					nearest_ci = ci ;
+			self.clusters[nearest_ci].append(data)
 
-class RBF:
-    def __init__(self, RBF_Func_number ):
-        self.RBF_Func_number = RBF_Func_number
-        self.kmean = Kmean(RBF_Func_number,RBF_Func_number)
-        self.m = RBF_Func_number
-        self.perceptron = perceptron(self.m)
-        # self.bias = np.random.random()
+		self.clusters = np.array(self.clusters)
+		for ci in range(self.cluster_num): 
+			if len(self.clusters[ci]) > 0:
+				self.C[ci] = self.centerOfMass(self.clusters[ci])
 
-    def fit(self , input_data):
-        self.kmean.train(input_data , iter_num = 10)
-        self.centerOfClusters = self.kmean.getCenterOfCluster()
-        print(self.centerOfClusters)
-        c = self.centerOfClusters
-        diff  = c[1]-c[0]
-        d = np.sqrt((diff[0]**2 + diff[1]**2))
-
-        # d = abs(max(self.centerOfClusters) - min(self.centerOfClusters))
-        self.sigma = d/(np.sqrt(2*self.m))
-        self.GaussianFunctions = []
-        self.centerOfClusters = np.array([[3,2] , [8,7] , [9,3] , [2,8]])
-        for c in self.centerOfClusters:
-            self.GaussianFunctions.append(Gaussian(c , self.sigma))
-
-    def train(self , x , d):
-        input_data_new = []
-        # print(x)
-        for input_data in x:
-            x_new = []
-            # print(input_data[0])
-            a1 = self.GaussianFunctions[0].eval(input_data)
-            a2 = self.GaussianFunctions[1].eval(input_data)
-            a3 = self.GaussianFunctions[2].eval(input_data)
-            a4 = self.GaussianFunctions[3].eval(input_data)
-            # a5 = self.GaussianFunctions[4].eval(input_data)
-            
-            # for i in range(self.m):
-            #     # F = F + self.GaussianFunctions[i](self.w[i] * self.input_data[i]) 
-                # x_new.append(self.GaussianFunctions[i].eval(input_data[i]))
-            input_data_new.append([a1,a2,a3,a4])
-            # input_data_new.append([a1,a2,a3,a4,a5])
-            # print([a1,a2])
-        # print(input_data_new)
-
-        x = np.array(x)
-        self.perceptron.train(input_data_new , d, x.shape[0])
-
-    def eval(self , data):
-        F = []
-        for input_data in data:
-            a1 = self.GaussianFunctions[0].eval(input_data)
-            a2 = self.GaussianFunctions[1].eval(input_data)
-            a3 = self.GaussianFunctions[2].eval(input_data)
-            a4 = self.GaussianFunctions[3].eval(input_data)
-            x_new = [a1,a2,a3,a4]
-            f = self.perceptron.eval(x_new)    
-               
-            F.append(f)
-        return np.array(F)
-
-if __name__=="__main__":
-    print("RBF ...")
-    x1 = [[1,8],[2,9],[3,8],[2,7],[3,6],[4,8],[6,4],[7,1],[7,3],[7,4],[7,5],[8,1],[8,2],[8,3],[9,1],[9,2],[9,4],[10,2],[10,3],[10,4]]
-    d1 = [1]*20
-    x2 = [[1,1],[1,3],[2,1],[2,2],[3,1],[3,2],[3,3],[4,2],[4,3],[4,4],[5,4],[5,5],[6,7],[7,6],[8,8],[8,9],[9,7],[9,8],[9,9],[10,10]]
-    d2 = [0]*20
-
-    x = x1+x2
-    d = d1+d2
-    x = np.array(x)
-    d = np.array(d)
-    rbf = RBF(4)
-    rbf.fit(x)
-    rbf.train(x,d)
-    print("end")
-    print("x = [1.1,8.1]" + str(rbf.eval([[1.1,8.1]]))) ## 1
-    print("x = [3.1,2.5]" + str(rbf.eval([[13.1,2.5]]))) ## 0
+	def getModifiedData(self):
+		output_data = [];
+		for data in self.input_data:
+			d , last_d = 0 , 100000000;
+			nearest_ci = 0;
+			for ci in range(self.cluster_num):
+				d = self.dist(data, self.C[ci])
+				if d < last_d:
+					last_d = d ;
+					nearest_ci = ci ;
+			output_data.append(self.C[nearest_ci])
+		return output_data
 
 
+	def getCenterOfCluster(self):
+		return self.C
 
-    # # Make data.
-    # X = np.arange(0, 9, 0.5)
-    # Y = np.arange(0, 9, 0.5)
-    # xx = []
-    # yy = []
-    # zz = []
 
-    # for i in X:
-    #     for j in Y:
-    #         xx.append(i)
-    #         yy.append(j)
-    #         zz.append(rbf.eval([[i,j]]))
+img = cv2.imread('test2.jpg')
+# print(img.size)
+kmean = Kmean(5,3);
+# inp_data = np.array([[0,0,0],[1,1,1.1],[1,1.1,1.2],[1.2,0.9,1],[1.1,1.1,0.9],[-0.1,0.1,-0.1],[-0.2,-0.1,0.1]]);
+img_vec = img.reshape(img.shape[0]*img.shape[1] , 3);
+print(img_vec.shape)
 
-    # xx = np.array(xx)
-    # yy = np.array(yy)
-    # zz = np.array(zz)
+# kmean.train(inp_data)
+# print(kmean.getCenterOfCluster())
 
-    # fig = plt.figure()
-    # ax = fig.gca(projection='3d')
+# kmean.train(inp_data)
+# print(kmean.getCenterOfCluster())
 
-    # # Plot the surface.
-    # ax.plot_wireframe(xx, yy, zz, rstride=10, cstride=10)
+kmean.train(img_vec)
+# print(np.array(kmean.getCenterOfCluster()))
+# print(np.array(np.uint8(kmean.getCenterOfCluster())))
+# print(kmean.getModifiedData())
+out_img = np.uint8(kmean.getModifiedData()).reshape(img.shape)
+print(out_img.shape)
+cv2.imshow("img", img)
+cv2.imshow("out_img", out_img)
+cv2.waitKey(0)
 
-    # plt.show()
+
